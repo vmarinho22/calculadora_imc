5c8d6a59f4b4496e304b12cf49b0c23ba9332e73
Vinicius Marinho <viniciusmarinho1616@gmail.com>
2021-09-01 22:10:40 -0300

    Layout Pronto

--- lib/main.dart
+++ lib/main.dart
@@ -17,9 +17,53 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Calculadora de IMC"),
-      ),
-    );
+        appBar: AppBar(
+          title: Text("Calculadora de IMC"),
+          centerTitle: true,
+          backgroundColor: Colors.green,
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.refresh),
+              onPressed: () {},
+            )
+          ],
+        ),
+        backgroundColor: Colors.white,
+        body: SingleChildScrollView(
+          padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+          child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Icon(Icons.person_outline, size: 120, color: Colors.green),
+                TextField(
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                        labelText: "Peso (Kg)",
+                        labelStyle: TextStyle(color: Colors.green)),
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.green, fontSize: 25)),
+                TextField(
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                        labelText: "Altura (cm)",
+                        labelStyle: TextStyle(color: Colors.green)),
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.green, fontSize: 25)),
+                Padding(
+                    padding: EdgeInsets.only(top: 10, bottom: 10),
+                    child: Container(
+                      height: 50,
+                      child: RaisedButton(
+                          onPressed: () {},
+                          child: Text("CALCULAR",
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 25)),
+                          color: Colors.green),
+                    )),
+                Text("Info",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.green, fontSize: 15)),
+              ]),
+        ));
   }
 }
